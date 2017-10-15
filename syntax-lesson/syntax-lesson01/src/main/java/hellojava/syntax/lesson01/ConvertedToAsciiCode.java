b297407b7618949e5ca28082041c28fd3944cccc
Tykkidream <tykkidream@gmail.com>
2017-10-15 15:28:05 +0800

    添加一些java语法学习代码

---
+++ syntax-lesson/syntax-lesson01/src/main/java/hellojava/syntax/lesson01/ConvertedToAsciiCode.java
@@ -0,0 +1,18 @@
+package hellojava.syntax.lesson01;
+
+
+public class ConvertedToAsciiCode {
+
+	public static void main(String[] args) {
+		char a = '1';
+		char b = 'A';
+
+		int i1 = (int)a;
+		int i2 = (int)b;
+		int i3 = Integer.valueOf(String.valueOf(a));
+
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+	}
+}
---
+++ syntax-lesson/syntax-lesson01/src/main/java/hellojava/syntax/lesson01/StrangeArithmetic.java
@@ -0,0 +1,16 @@
+package hellojava.syntax.lesson01;
+
+/**
+ * Created by Tykkidream on 2017/10/6.
+ */
+public class StrangeArithmetic {
+    public static void main(String[] args) {
+        double a =  0.1;
+
+        double b = 0.2;
+
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a * 3);
+    }
+}
