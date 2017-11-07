38917696fa3d69dd5648307b100d9f520cac8d2e
Tykkidream <tykkidream@gmail.com>
2017-11-07 21:04:20 +0800

    实现学习读写抢锁，添加日志观察读写锁等待情况

--- concurrent-lesson/concurrent-lesson02/src/main/java/hellojava/concurrent/lesson02/ReadWriteDemo.java
+++ concurrent-lesson/concurrent-lesson02/src/main/java/hellojava/concurrent/lesson02/ReadWriteDemo.java
@@ -3,6 +3,7 @@ package hellojava.concurrent.lesson02;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,9 +11,27 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * Created by Tykkidream on 2017/11/5.
  */
 public class ReadWriteDemo {
+    private static Logger logger = LoggerFactory.getLogger(ReadReadDemo.class);
 
-    public static void main(String[] args) {
-        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    public static class ReentrantReadWriteLock_Public extends ReentrantReadWriteLock {
+        @Override
+        public Collection<Thread> getQueuedThreads() {
+            return super.getQueuedThreads();
+        }
+
+        @Override
+        protected Collection<Thread> getQueuedReaderThreads() {
+            return super.getQueuedReaderThreads();
+        }
+
+        @Override
+        protected Collection<Thread> getQueuedWriterThreads() {
+            return super.getQueuedWriterThreads();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantReadWriteLock_Public readWriteLock = new ReentrantReadWriteLock_Public();
 
         // 试验读读共享的情况
         ReentrantReadWriteLock.ReadLock readLock = readWriteLock.readLock();
@@ -27,17 +46,24 @@ public class ReadWriteDemo {
         ReentrantReadWriteLock.WriteLock writeLock = readWriteLock.writeLock();
         WriteLockBusinessThread thread6 = new WriteLockBusinessThread(writeLock);
 
-
-
-
         thread1.start();
         thread2.start();
 
         thread6.start();
 
+        Thread.sleep(100);
+
+        logger.info("可能正在等待获取【读取】锁的线程：{}", readWriteLock.getQueuedReaderThreads());
+        logger.info("可能正在等待获取【写入】锁的线程：{}", readWriteLock.getQueuedWriterThreads());
+
         thread3.start();
         thread4.start();
         thread5.start();
+
+        Thread.sleep(100);
+
+        logger.info("可能正在等待获取【读取】锁的线程：{}", readWriteLock.getQueuedReaderThreads());
+        logger.info("可能正在等待获取【写入】锁的线程：{}", readWriteLock.getQueuedWriterThreads());
     }
 
     public static class ReadLockBusinessThread extends Thread {
@@ -55,12 +81,12 @@ public class ReadWriteDemo {
             lock.lock();
 
             try {
-                logger.info(String.format("线程 %s 抢到到【读】锁！", currentThread().getName()));
-                Thread.sleep(1000);
+                logger.info("线程 {} 抢到到【读】锁！", currentThread().getName());
+                Thread.sleep(2000);
             } catch (Throwable throwable) {
                 logger.error(throwable.getMessage(), throwable);
             } finally {
-                logger.info(String.format("线程 %s 处理完成！", currentThread().getName()));
+                logger.info("线程 {} 处理完成！", currentThread().getName());
                 lock.unlock();
             }
         }
@@ -81,12 +107,12 @@ public class ReadWriteDemo {
             lock.lock();
 
             try {
-                logger.info(String.format("线程 %s 抢到到【写】锁！", currentThread().getName()));
-                Thread.sleep(1000);
+                logger.info("线程 {} 抢到到【写】锁！", currentThread().getName());
+                Thread.sleep(2000);
             } catch (Throwable throwable) {
                 logger.error(throwable.getMessage(), throwable);
             } finally {
-                logger.info(String.format("线程 %s 处理完成！", currentThread().getName()));
+                logger.info("线程 {} 处理完成！", currentThread().getName());
                 lock.unlock();
             }
         }
