8cd3bfd6a8e984bee9c58a1d92ec0789abe164cc
wuliqing <wuliqing@quanqiuwa.com>
2022-03-03 11:23:09 +0800

    学习 computeIfAbsent

---
+++ util-lesson/util-lesson01/src/main/java/hellojava/util/lesson01/MapDemo.java
@@ -0,0 +1,61 @@
+package hellojava.util.lesson01;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<String, List> map = new HashMap<>();
+
+        System.out.println(map.toString());
+
+        {
+            List list = map.get("a");
+
+            if (list == null) {
+                list = new LinkedList();
+
+                map.put("a", list);
+            }
+
+            list.add("abc");
+            list.add("123");
+        }
+
+        System.out.println(map.toString());
+
+        {
+            List list = map.computeIfAbsent("b", MapDemo::create);
+
+            list.add("abc");
+            list.add("123");
+        }
+
+        System.out.println(map.toString());
+
+        {
+            List list = map.computeIfAbsent("b", MapDemo::create);
+
+            list.add("qwe");
+            list.add("!@#");
+        }
+
+        System.out.println(map.toString());
+
+        {
+            List list = map.computeIfAbsent("c", MapDemo::create);
+
+            list.add("111");
+            list.add("222");
+        }
+
+        System.out.println(map.toString());
+    }
+
+    private static List create(String key) {
+        System.out.println("创建一个新的 list ， key = " + key);
+        return new LinkedList();
+    }
+}
