064cede046461b54e44a7b651388f69990d8bcb2
tykkidream <tykkidream@163.com>
2024-07-07 00:38:28 +0800

    使用虚拟线程

---
+++ v21-lesson/v21-lesson-concurrent/README.md
@@ -0,0 +1,4 @@
+说明
+================================
+
+- [Demo01.java](src%2Fmain%2Fjava%2Fhellojava%2Fv21%2Fconcurrent%2FDemo01.java): 简单使用虚拟线程。
---
+++ v21-lesson/v21-lesson-concurrent/src/main/java/hellojava/v21/concurrent/Demo01.java
@@ -0,0 +1,37 @@
+package hellojava.v21.concurrent;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Demo01 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Demo01.class);
+
+    /**
+     * 简单使用虚拟线程
+     *
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Thread.ofVirtual().start(() -> {
+            logger.info("在虚拟线程中执行开始");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            logger.info("在虚拟线程中执行结束");
+        });
+
+        logger.info("开始");
+
+        Thread.sleep(1000);
+        logger.info("中间");
+        Thread.sleep(1000);
+
+        logger.info("结束");
+
+    }
+}
